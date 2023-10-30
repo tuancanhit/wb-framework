74afd8861f705ca88cc626cbcb1a3bcc06f824ef
Nguyen Tuan Canh <canhnt@smartosc.com>
2023-10-30 15:57:39 +0700

    Init structure

--- index.php
+++ index.php
@@ -16,23 +16,4 @@ try {
         HTML;
     exit(1);
 }
-
-use GuzzleHttp\Client;
-use GuzzleHttp\Promise;
-
-$client = new Client(['base_uri' => 'http://127.0.0.1']);
-
-// Initiate each request but do not block
-$promises = [
-    'image' => $client->getAsync('/?type=image'),
-    'png'   => $client->getAsync('/?type=png'),
-    'jpeg'  => $client->getAsync('/?type=jpeg'),
-    'webp'  => $client->getAsync('/?type=webp')
-];
-
-$responses = Promise\Utils::unwrap($promises);
-
-foreach ($responses as $key => $response) {
-    echo $key .': '.$response->getBody()->getContents();
-    echo "\n";
-}
+echo "FW is developing......\n";
